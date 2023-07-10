bddd2bb71a36bb5da5394788a5a6bca72e65e681
raniamo8 <raneaxxl@gmail.com>
2023-07-10 14:43:42 +0200

    deletes

--- app/src/main/java/com/example/customerapp/CodeFragment.java
+++ app/src/main/java/com/example/customerapp/CodeFragment.java
@@ -13,11 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CodeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CodeFragment extends Fragment {
 
     private EditText personName;
@@ -25,17 +21,6 @@ public class CodeFragment extends Fragment {
     private Button buttonGenerate;
 
     private ImageView qrCode;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public CodeFragment() {
         // Required empty public constructor
     }
@@ -43,12 +28,8 @@ public class CodeFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CodeFragment.
      */
-    // TODO: Rename and change types and number of parameters
+    /*
     public static CodeFragment newInstance(String param1, String param2) {
         CodeFragment fragment = new CodeFragment();
         Bundle args = new Bundle();
@@ -57,14 +38,11 @@ public class CodeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    */
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Nullable
