b9701de5239ecfe2a065e9d4aca093e6f0d14e16
raniamo8 <raneaxxl@gmail.com>
2023-07-13 22:46:56 +0200

    new files

---
+++ app/src/main/java/com/example/customerapp/Address.java
@@ -0,0 +1,4 @@
+package com.example.customerapp;
+
+public class Address {
+}
---
+++ app/src/test/java/com/example/customerapp/AddressUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.customerapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AddressUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
