2fdac8be0786e8bad1eff9c1252e4f7cf4217d87
raniamo8 <raneaxxl@gmail.com>
2023-07-25 00:56:11 +0200

    TODO's

--- app/src/main/java/com/example/customerapp/MainActivity.java
+++ app/src/main/java/com/example/customerapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.customerapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -10,18 +12,28 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.customerapp.databinding.ActivitymainBinding;
 
+
+//TODO: fix the problem with the display of the name after reopening the app
+//TODO: test the app on real device
 public class MainActivity extends AppCompatActivity {
+    private static final String CURRENT_FRAGMENT_TAG = "current_fragment_tag";
+    private Fragment currentFragment;
     ActivitymainBinding binding;
     private Toolbar toolbar;
+    private AddressBook addressBook;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitymainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Log.d("MainActivity", "onCreate");
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        addressBook = AddressBook.getInstance();
+        addressBook.loadData(getApplicationContext());
 
         if (savedInstanceState == null) {
             replaceFragment(new QRCodeListFragment());
@@ -41,11 +53,18 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+    }
 
-
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (currentFragment != null) {
+            getSupportFragmentManager().putFragment(outState, CURRENT_FRAGMENT_TAG, currentFragment);
+        }
     }
 
     private void replaceFragment(Fragment fragment){
+        currentFragment = fragment;
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
@@ -55,5 +74,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+        addressBook.saveData(getApplicationContext());
+    }
 }
 
