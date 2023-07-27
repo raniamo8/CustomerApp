9a374503678d19a70e9e6b9576424365b207dd5b
raniamo8 <raneaxxl@gmail.com>
2023-07-27 17:32:25 +0200

    deletAllButton in SettingFragment

--- app/src/main/java/com/example/customerapp/SettingFragment.java
+++ app/src/main/java/com/example/customerapp/SettingFragment.java
@@ -7,28 +7,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SettingFragment extends Fragment {
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch darkModeSwitch;
+    Button deleteAllButton, addAddressButton;
     boolean nightMode;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+    private List<String> qrCodeFilePaths;
+    private QRCodeAdapter qrCodeAdapter;
+    private AddressBook addressBook;
+
 
     public SettingFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        qrCodeFilePaths = new ArrayList<>();
+        addressBook = AddressBook.getInstance();
+        addressBook.loadData(getContext());
+        qrCodeAdapter = new QRCodeAdapter(getContext(), qrCodeFilePaths, addressBook);
+        setHasOptionsMenu(true);
     }
 
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        addressBook = AddressBook.getInstance();
+
         darkModeSwitch = view.findViewById(R.id.darkModeSwitch);
         sharedPreferences = Objects.requireNonNull(getActivity()).getSharedPreferences("MODE", Context.MODE_PRIVATE);
         nightMode = sharedPreferences.getBoolean("night", false);
@@ -36,6 +59,17 @@ public class SettingFragment extends Fragment {
             darkModeSwitch.setChecked(true);
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }
+
+
+
+        deleteAllButton = view.findViewById(R.id.deleteAllButton);
+        deleteAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteAllQRandRecipients();
+            }
+        });
+
 
         darkModeSwitch.setOnClickListener(view1 -> {
             nightMode = !nightMode;
@@ -54,5 +88,18 @@ public class SettingFragment extends Fragment {
         return view;
     }
 
+    private void goToCodeFragment() {
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, CodeFragment.getInstance());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void deleteAllQRandRecipients() {
+        addressBook.deleteAllRecipients(getContext());
+        Toast.makeText(getContext(), "Alle QR-Codes wurden gelöscht.", Toast.LENGTH_SHORT).show();
+    }
 
 }
