d58faa983bb06ab555dbc5dfced57750229e1047
raniamo8 <raneaxxl@gmail.com>
2023-07-28 14:10:58 +0200

    StoreDetails xml

--- app/src/main/java/com/example/customerapp/StoreDetails.java
+++ app/src/main/java/com/example/customerapp/StoreDetails.java
@@ -2,4 +2,60 @@ package com.example.customerapp;
 
 //TODO: define the class and its attributes
 public class StoreDetails {
+
+    private String shopname;
+    private String owner;
+    private Address address;
+    private String phonenumber;
+    private String email;
+
+    public StoreDetails(String shopname, String owner, Address address, String phonenumber, String email) {
+        this.shopname = shopname;
+        this.owner = owner;
+        this.address = address;
+        this.phonenumber = phonenumber;
+        this.email = email;
+    }
+
+    public String getShopname() {
+        return shopname;
+    }
+
+    public void setShopname(String shopname) {
+        this.shopname = shopname;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
