041abcc74f5f3ad45fd31ca5875f8b24d058703c
raniamo8 <raneaxxl@gmail.com>
2023-08-09 19:14:14 +0200

    welcomefragment 1 first steps

--- app/src/main/java/com/example/customerapp/WelcomeFragmentOne.java
+++ app/src/main/java/com/example/customerapp/WelcomeFragmentOne.java
@@ -1,5 +1,6 @@
 package com.example.customerapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class WelcomeFragmentOne extends Fragment {
 
+    private TextView welcomeTextView, info1TextView;
+    private ImageView intro1;
+    Button nextButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_one, container, false);
+        View welcomeView = inflater.inflate(R.layout.fragment_welcome_one, container, false);
+
+        welcomeTextView = welcomeView.findViewById(R.id.welcomeTextView);
+        info1TextView = welcomeView.findViewById(R.id.info1TextView);
+        intro1 = welcomeView.findViewById(R.id.intro1);
+        nextButton = welcomeView.findViewById(R.id.nextButton);
+
+
+
+
+        return welcomeView;
     }
 }
