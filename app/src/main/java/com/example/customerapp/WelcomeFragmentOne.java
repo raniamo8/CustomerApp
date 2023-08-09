54a9ad6d078fbb8d914cd28e47fdb1afd7af3bdd
raniamo8 <raneaxxl@gmail.com>
2023-08-09 20:35:54 +0200

    code refactor

--- app/src/main/java/com/example/customerapp/WelcomeFragmentOne.java
+++ app/src/main/java/com/example/customerapp/WelcomeFragmentOne.java
@@ -1,40 +1,30 @@
 package com.example.customerapp;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.Objects;
 
 
 public class WelcomeFragmentOne extends Fragment {
 
-    private Button nextButton;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View welcomeView = inflater.inflate(R.layout.fragment_welcome_one, container, false);
 
-        nextButton = welcomeView.findViewById(R.id.nextButton);
+        Button nextButton = welcomeView.findViewById(R.id.nextButton);
 
-        // Setze einen OnClickListener für den Button, der WelcomeFragmentTwo öffnet
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment fragment = new WelcomeFragmentTwo();
-                Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.welcome_frame_layout, fragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
+        nextButton.setOnClickListener(v -> {
+            Fragment fragment = new WelcomeFragmentTwo();
+            Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.welcome_frame_layout, fragment)
+                    .addToBackStack(null)
+                    .commit();
         });
 
         return welcomeView;
--- app/src/main/java/com/example/customerapp/WelcomeFragmentTwo.java
+++ app/src/main/java/com/example/customerapp/WelcomeFragmentTwo.java
@@ -1,43 +1,30 @@
 package com.example.customerapp;
 
-import static android.content.Context.MODE_PRIVATE;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.Objects;
 
 
 public class WelcomeFragmentTwo extends Fragment {
 
-    private Button introAddAddressButton;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View welcomeView = inflater.inflate(R.layout.fragment_welcome_two, container, false);
 
-        introAddAddressButton = welcomeView.findViewById(R.id.introAddAddressButton);
+        Button introAddAddressButton = welcomeView.findViewById(R.id.introAddAddressButton);
 
-        // Setze einen OnClickListener für den Button, der die nächste Activity oder das Fragment öffnet
-        introAddAddressButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
+        introAddAddressButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
 
-                getActivity().finish();
-            }
+            Objects.requireNonNull(getActivity()).finish();
         });
 
         return welcomeView;
