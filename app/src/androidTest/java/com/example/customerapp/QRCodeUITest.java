12286db9df0eb3df4c0e4332e918d20dfac6cfb6
raniamo8 <raneaxxl@gmail.com>
2023-08-11 17:38:46 +0200

    backButton Ui test

--- app/src/androidTest/java/com/example/customerapp/QRCodeUITest.java
+++ app/src/androidTest/java/com/example/customerapp/QRCodeUITest.java
@@ -97,18 +97,28 @@ public class QRCodeUITest {
     }
 
     @Test
-    public void test6AddTwoQRCodes() {
+    public void test6BackButton() {
+        onView(isRoot()).perform(waitFor(1000));
+        onView(withId(R.id.fabAddQRCode)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.backButton)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.settings)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.addAddressButton)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.backButton)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.code)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+    }
+
+    @Test
+    public void test7AddTwoQRCodesOnTwoFragments() {
         onView(isRoot()).perform(waitFor(1000));
         onView(withId(R.id.fabAddQRCode)).perform(click());
         onView(withId(R.id.lastNameEditText)).perform(typeText("Mo"), closeSoftKeyboard());
-        onView(withId(R.id.firstNameEditText)).perform(typeText("R"), closeSoftKeyboard());
-        onView(withId(R.id.streetEditText)).perform(typeText("Teststr"), closeSoftKeyboard());
-        onView(withId(R.id.streetNrEditText)).perform(typeText("10C"), closeSoftKeyboard());
-        onView(isRoot()).perform(waitFor(2000));
-        onView(withId(R.id.buttonGenerate)).perform(click());
-        onView(isRoot()).perform(waitFor(2000));
-        onView(withId(R.id.lastNameEditText)).perform(typeText("Ps"), closeSoftKeyboard());
-        onView(withId(R.id.firstNameEditText)).perform(typeText("T"), closeSoftKeyboard());
+        onView(withId(R.id.firstNameEditText)).perform(typeText("Rania"), closeSoftKeyboard());
         onView(withId(R.id.streetEditText)).perform(typeText("Teststr"), closeSoftKeyboard());
         onView(withId(R.id.streetNrEditText)).perform(typeText("10C"), closeSoftKeyboard());
         onView(isRoot()).perform(waitFor(2000));
@@ -116,15 +126,31 @@ public class QRCodeUITest {
         onView(isRoot()).perform(waitFor(2000));
         onView(withId(R.id.backButton)).perform(click());
         onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.settings)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.addAddressButton)).perform(click());
+        onView(isRoot()).perform(waitFor(500));
+        onView(withId(R.id.lastNameEditText)).perform(typeText("Baj"), closeSoftKeyboard());
+        onView(withId(R.id.firstNameEditText)).perform(typeText("Sadik"), closeSoftKeyboard());
+        onView(withId(R.id.streetEditText)).perform(typeText("Teststr"), closeSoftKeyboard());
+        onView(withId(R.id.streetNrEditText)).perform(typeText("10C"), closeSoftKeyboard());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.buttonGenerate)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.backButton)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.code)).perform(click());
+        onView(isRoot()).perform(waitFor(2000));
     }
 
     @Test
-    public void test7DeleteAllQRCode() {
+    public void test8DeleteAllQRCode() {
         onView(isRoot()).perform(waitFor(1000));
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         onView(withText("Alle QR-Codes löschen")).perform(click());
         onView(isRoot()).perform(waitFor(2000));
     }
+
 
     public static ViewAction waitFor(final long millis) {
         return new ViewAction() {
