daf93cb6bd377f9fe0f04e0e0981e084a547b979
raniamo8 <raneaxxl@gmail.com>
2023-08-12 12:11:06 +0200

    comment

--- app/src/test/java/com/example/customerapp/UnitTestSuite.java
+++ app/src/test/java/com/example/customerapp/UnitTestSuite.java
@@ -3,6 +3,10 @@ package com.example.customerapp;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * This is the test suite to run all UnitTests
+ */
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AddressBookUnitTest.class,
