ea5effe4d5a123cb11ad0cf438d77cbc34c020ca
raniamo8 <raneaxxl@gmail.com>
2023-08-14 20:49:30 +0200

    refactor

--- app/src/main/java/com/example/customerapp/SettingFragment.java
+++ app/src/main/java/com/example/customerapp/SettingFragment.java
@@ -1,7 +1,6 @@
 package com.example.customerapp;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
