3b0efca8993a411e2a2171ceb196b3dc3afe1b14
raniamo8 <raneaxxl@gmail.com>
2023-08-18 13:31:50 +0200

    back button in the intro

--- app/src/main/java/com/example/customerapp/WelcomeFragmentTwo.java
+++ app/src/main/java/com/example/customerapp/WelcomeFragmentTwo.java
@@ -1,18 +1,23 @@
 package com.example.customerapp;
 
+import static com.example.customerapp.FragmentManagerHelper.goBackToPreviousFragment;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 /**
  * Second fragment of the introduction.
  */
 public class WelcomeFragmentTwo extends Fragment {
+    ImageButton backToIntroButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View welcomeView = inflater.inflate(R.layout.fragment_welcome_two, container, false);
@@ -23,6 +28,13 @@ public class WelcomeFragmentTwo extends Fragment {
 
             requireActivity().finish();
         });
+
+        backToIntroButton = welcomeView.findViewById(R.id.backToIntroButton);
+        backToIntroButton.setOnClickListener(v -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            goBackToPreviousFragment(fragmentManager);
+        });
+
         return welcomeView;
     }
 }
