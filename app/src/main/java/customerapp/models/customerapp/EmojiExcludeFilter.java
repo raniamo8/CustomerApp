af30256d811abd56437fab0847ec325e6e6d5096
raniamo8 <122220921+raniamo8@users.noreply.github.com>
2023-09-07 11:41:03 +0200

    java doc for EmojiExcludeFilter

--- app/src/main/java/customerapp/models/customerapp/EmojiExcludeFilter.java
+++ app/src/main/java/customerapp/models/customerapp/EmojiExcludeFilter.java
@@ -4,7 +4,23 @@ import android.text.InputFilter;
 import android.text.SpannableString;
 import android.text.Spanned;
 
+/**
+ * An input filter that excludes emojis and surrogate characters.
+ * Use this filter to prevent users from entering such characters in text fields.
+ */
 public class EmojiExcludeFilter implements InputFilter {
+    
+    /**
+     * Filters out emojis and surrogate characters from the input.
+     *
+     * @param source The original characters being input.
+     * @param start  The start of the range of source characters.
+     * @param end    The end of the range of source characters.
+     * @param dest   The current content.
+     * @param dstart The start of the range of destination characters that are being replaced by the new text.
+     * @param dend   The end of the range of destination characters that are being replaced by the new text.
+     * @return The filtered character sequence without emojis and surrogate characters. 
+     */
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
         StringBuilder filtered = new StringBuilder();
